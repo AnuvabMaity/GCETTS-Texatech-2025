31e28a4d26641e1058841978a732ae9f43609ed1
AnuvabMaity <anuvabmaity99@gmail.com>
2025-04-26 14:09:07 +0530

    Updated Event Links

--- src/utils/events.js
+++ src/utils/events.js
@@ -1,62 +1,50 @@
 export const events = [
     {
         "event_name": "INNOVA",
-        "description": "An AI Hackathon - Build innovative tech solutions in a team during a 24-hour coding sprint.",
+        "description": "An AI Hackathon - Build innovative tech solutions in a team during a 12-hour coding sprint.",
         "brochure_link": "https://drive.google.com/file/d/1zhph7rkinVVoUVFrX17h0TRAyvHtmA8u/view",
         "gform_link": "https://unstop.com/o/FjMGzVv?utm_medium=Share&utm_source=shortUrl"
     },
     {
         "event_name": "LOGIQ",
         "description": "The Coding Context of Texatech - Test your programming skills and problem-solving under time pressure.",
-        "brochure_link": "https://gcettstexatech.vercel.app/images/comingsoon.png",
-        "gform_link": "https://gcettstexatech.vercel.app/images/comingsoon.png"
+        "brochure_link": "https://drive.google.com/file/d/1oXwQYDPxhanMPhd6vOLZu0kTDWWGLWeM/view?usp=sharing",
+        "gform_link": "https://docs.google.com/forms/d/e/1FAIpQLSel-3zitJZAh1SfmZIo5RIzQ1jC3FCECTqN99pRrHSnba7RcQ/viewform?usp=sharing"
     },
     {
-        "event_name": "Extempore",
+        "event_name": "UNSCRIPTED",
         "description": "Impromptu speech competition to showcase your spontaneity and clarity of thought.",
-        "brochure_link": "https://gcettstexatech.vercel.app/images/comingsoon.png",
-        "gform_link": "https://gcettstexatech.vercel.app/images/comingsoon.png"
+        "brochure_link": "https://drive.google.com/file/d/1UuUOLSon3mywq-wrTajCFTXJxdtCTuMe/view?usp=drive_link",
+        "gform_link": "https://docs.google.com/forms/d/e/1FAIpQLSf_yT3ozVyIKCIhk-_JjDpOeY8jYnRPQSOK7h5Tqh6Oxi7xMA/viewform"
     },
     {
-        "event_name": "Debate",
+        "event_name": "DEBATTLE",
         "description": "Engage in fiery discussions and present strong arguments on current topics.",
-        "brochure_link": "https://gcettstexatech.vercel.app/images/comingsoon.png",
-        "gform_link": "https://gcettstexatech.vercel.app/images/comingsoon.png"
+        "brochure_link": "https://drive.google.com/file/d/1E7sF7V9G7GcN6m0RsDah5ESCmq00aNsv/view?usp=sharing",
+        "gform_link": "https://docs.google.com/forms/d/e/1FAIpQLSctF2XQNvGn-tnSQo9C3HloUK5TMBuFeNSXkXajxgm0ckjHfg/viewform"
     },
     {
-        "event_name": "Recitation",
+        "event_name": "ELOQUENCE",
         "description": "Recite poetry with emotion and flair in this literary showcase.",
-        "brochure_link": "https://gcettstexatech.vercel.app/images/comingsoon.png",
-        "gform_link": "https://gcettstexatech.vercel.app/images/comingsoon.png"
+        "brochure_link": "https://drive.google.com/file/d/1xHnZSFTAX8qwBdiYPNqi1gwL_OHXWqWu/view?usp=sharing",
+        "gform_link": "https://docs.google.com/forms/d/e/1FAIpQLScjCZ7dHCGNiaGoexaQzT9BT_wFGpm-ZJnorHTi7KJg3lcIAg/viewform"
     },
     {
-        "event_name": "Sudoku",
-        "description": "Prove your logical reasoning in a competitive Sudoku challenge.",
-        "brochure_link": "https://gcettstexatech.vercel.app/images/comingsoon.png",
-        "gform_link": "https://gcettstexatech.vercel.app/images/comingsoon.png"
-    },
-    {
-        "event_name": "Paper Presentation",
-        "description": "Present your research and innovative ideas in front of a jury panel.",
-        "brochure_link": "https://gcettstexatech.vercel.app/images/comingsoon.png",
-        "gform_link": "https://gcettstexatech.vercel.app/images/comingsoon.png"
-    },
-    {
-        "event_name": "Chess",
+        "event_name": "CHESS",
         "description": "Showcase your strategy and planning in this classic mind sport.",
-        "brochure_link": "https://gcettstexatech.vercel.app/images/comingsoon.png",
-        "gform_link": "https://gcettstexatech.vercel.app/images/comingsoon.png"
+        "brochure_link": "https://drive.google.com/file/d/1-wTFKfBZJwzpe5Cfi9GzK3EvLTOAGgFj/view?usp=sharing",
+        "gform_link": "https://docs.google.com/forms/d/e/1FAIpQLSe9X1SHjckd2EbrFun1C-Iqxwdmt9etQo9CzHhB4OkGU7GAwg/viewform"
     },
     {
-        "event_name": "Drawing",
+        "event_name": "COLOURSPLASH",
         "description": "Let your creativity shine through art in this drawing competition.",
-        "brochure_link": "https://gcettstexatech.vercel.app/images/comingsoon.png",
-        "gform_link": "https://gcettstexatech.vercel.app/images/comingsoon.png"
+        "brochure_link": "https://drive.google.com/file/d/12jhMOjMD0yHsNddtQx0-O1AHVuq5NBTa/view?usp=sharing",
+        "gform_link": "https://docs.google.com/forms/d/e/1FAIpQLSflelo2B5w533gvpH-WcewSWEZNsmEYRufly6NEb3h7SWn-ng/viewform"
     },
     {
-        "event_name": "Aptitude",
+        "event_name": "THINKFAST",
         "description": "Solve numerical and logical puzzles under a time constraint.",
-        "brochure_link": "https://gcettstexatech.vercel.app/images/comingsoon.png",
-        "gform_link": "https://gcettstexatech.vercel.app/images/comingsoon.png"
+        "brochure_link": "https://drive.google.com/file/d/1Aq9Eo6GkKsYnf1KqbNxNo0xoCf4hZwby/view?usp=sharing",
+        "gform_link": "https://docs.google.com/forms/d/e/1FAIpQLSeRCKT3zFa-4L7s-yr8ili_mNLQA_8kvBmiKXAwh8bWLyUP4w/viewform"
     }
 ]
